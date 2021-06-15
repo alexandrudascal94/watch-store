85fcb90388e73f70d44af23320c678db4ea885af
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-15 20:29:02 +0200

    [dev] Add basket controller with plain checkout endpoint

---
+++ src/main/java/com/maha/watchstore/controller/BasketController.java
@@ -0,0 +1,18 @@
+package com.maha.watchstore.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/")
+public class BasketController {
+
+    @PostMapping("checkout")
+    public ResponseEntity<Long> checkout(List<Long> itemIds){
+        return ResponseEntity.ok().body(300L);
+    }
+}
