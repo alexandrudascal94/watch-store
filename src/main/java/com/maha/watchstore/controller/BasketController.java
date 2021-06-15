3094e977866f1d27fd5c22b2501c9b249f0f36e0
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-15 20:45:25 +0200

    [test] Add test for checkout with empty basket

--- src/main/java/com/maha/watchstore/controller/BasketController.java
+++ src/main/java/com/maha/watchstore/controller/BasketController.java
@@ -2,6 +2,7 @@ package com.maha.watchstore.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import java.util.List;
 public class BasketController {
 
     @PostMapping("checkout")
-    public ResponseEntity<Long> checkout(List<Long> itemIds){
+    public ResponseEntity<Long> checkout(@RequestBody List<Long> itemIds){
         return ResponseEntity.ok().body(300L);
     }
 }
--- src/test/java/com/maha/watchstore/controller/BasketControllerTest.java
+++ src/test/java/com/maha/watchstore/controller/BasketControllerTest.java
@@ -3,7 +3,12 @@ package com.maha.watchstore.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(BasketController.class)
 public class BasketControllerTest {
@@ -12,7 +17,11 @@ public class BasketControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void test(){
-
+    public void noItemsCheckout_ShouldReturn_BadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/checkout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[]"))
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 }
