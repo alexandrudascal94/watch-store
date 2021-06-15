6db9b3e42122193b4f4b6a87c9710983b1b67e71
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-15 20:27:06 +0200

    [dev] Init spring-boot app

---
+++ src/test/java/com/maha/watchstore/WatchStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.maha.watchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
