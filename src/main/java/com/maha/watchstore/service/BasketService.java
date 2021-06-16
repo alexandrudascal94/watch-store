f80e9f133297ed43529e74ed4d1af0208fbb0088
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-16 11:35:11 +0200

    [dev] Add logic to calculate the price based on Product entities

--- src/main/java/com/maha/watchstore/service/BasketService.java
+++ src/main/java/com/maha/watchstore/service/BasketService.java
@@ -1,14 +1,23 @@
 package com.maha.watchstore.service;
 
 
+import com.maha.watchstore.exception.UnsupportedBasketItemsException;
+import com.maha.watchstore.respository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class BasketService {
+
+    private final ProductRepository productRepository;
+
     public Long calculatePriceFor(List<Long> itemIds) {
-        return 200L;
-       //throw new UnsupportedBasketItemsException("ItemIds list is empty");
+        return itemIds.stream().mapToLong(id -> productRepository.findById(id)
+                .orElseThrow(() -> new UnsupportedBasketItemsException("One item could not be find"))
+                .getPrice())
+                .sum();
     }
 }
