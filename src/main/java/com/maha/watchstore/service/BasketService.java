fe2a6906a20452fb3faaa9dcd2b5d80fd3810f46
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-16 19:50:37 +0200

    [dev] Enhanced calculate price logic to avoid request the same product multiple time

--- src/main/java/com/maha/watchstore/service/BasketService.java
+++ src/main/java/com/maha/watchstore/service/BasketService.java
@@ -1,13 +1,18 @@
 package com.maha.watchstore.service;
 
-
+import com.maha.watchstore.entity.Product;
 import com.maha.watchstore.exception.NonExistentProductException;
-import com.maha.watchstore.exception.UnsupportedItemException;
 import com.maha.watchstore.respository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.function.UnaryOperator.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 @Service
 @AllArgsConstructor
@@ -16,9 +21,16 @@ public class BasketService {
     private final ProductRepository productRepository;
 
     public Long calculatePriceFor(List<Long> itemIds) {
-        return itemIds.stream().mapToLong(id -> productRepository.findById(id)
-                .orElseThrow(() -> new NonExistentProductException("The product does not exist"))
-                .getPrice())
+        Map<Long, Long> itemsCountMap = itemIds.stream().collect(groupingBy(identity(), counting()));
+
+        return itemsCountMap.keySet()
+                .stream()
+                .mapToLong(id -> getPriceWithDiscount(productRepository.findById(id), itemsCountMap.get(id)))
                 .sum();
+    }
+
+    private long getPriceWithDiscount(Optional<Product> product, Long count) {
+        final long productPrice = product.orElseThrow(() -> new NonExistentProductException("The product does not exist")).getPrice();
+        return productPrice * count;
     }
 }
