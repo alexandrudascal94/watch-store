94ffd335e1af027c882358675ed6478976c14193
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-16 11:35:37 +0200

    [dev] Code rearranging

--- src/main/java/com/maha/watchstore/exception/UnsupportedBasketItemsException.java
+++ src/main/java/com/maha/watchstore/exception/UnsupportedBasketItemsException.java
@@ -3,6 +3,7 @@ package com.maha.watchstore.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnsupportedBasketItemsException extends RuntimeException {
     public UnsupportedBasketItemsException(String message) {
--- src/test/java/com/maha/watchstore/service/BasketServiceTest.java
+++ src/test/java/com/maha/watchstore/service/BasketServiceTest.java
@@ -1,12 +1,9 @@
 package com.maha.watchstore.service;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Assertions;
-
-
 
 import java.util.List;
 
