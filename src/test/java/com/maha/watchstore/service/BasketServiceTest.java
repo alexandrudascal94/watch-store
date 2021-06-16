4fd86852383708c81d9e105f157b7290347259bc
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-16 18:07:48 +0200

    [test] Add ProductRepository mock

--- src/test/java/com/maha/watchstore/service/BasketServiceTest.java
+++ src/test/java/com/maha/watchstore/service/BasketServiceTest.java
@@ -1,14 +1,23 @@
 package com.maha.watchstore.service;
 
+import com.maha.watchstore.entity.Product;
+import com.maha.watchstore.respository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {BasketService.class})
 public class BasketServiceTest {
+
+    @MockBean
+    private ProductRepository productRepositoryMock;
 
     @Autowired
     private BasketService serviceUnderTest;
@@ -20,7 +29,14 @@ public class BasketServiceTest {
 
     @Test
     public void calculatePriceFor_multipleDifferentItemsNoDiscount_ShouldReturn_totalPricePrice() {
-        Assertions.assertEquals(300L, serviceUnderTest.calculatePriceFor(List.of(1L, 2L, 3L)));
+        Product productOne = new Product();
+        productOne.setPrice(100L);
+        Product productTwo = new Product();
+        productTwo.setPrice(200L);
+        when(productRepositoryMock.findById(1L)).thenReturn(Optional.of(productOne));
+        when(productRepositoryMock.findById(2L)).thenReturn(Optional.of(productTwo));
+
+        Assertions.assertEquals(300L, serviceUnderTest.calculatePriceFor(List.of(1L, 2L)));
     }
 }
 
