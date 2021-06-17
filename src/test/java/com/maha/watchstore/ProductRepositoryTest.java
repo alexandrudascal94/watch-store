24b13306b173e557fab10c163488e0aa789ac140
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-17 08:31:26 +0200

    [test] Add repository test for findById() to return correct product

---
+++ src/test/java/com/maha/watchstore/ProductRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.maha.watchstore;
+
+import com.maha.watchstore.entity.Product;
+import com.maha.watchstore.respository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void getProductById_should_returnCorrespondingProduct() {
+        Optional<Product> expectedProduct = productRepository.findById(1L);
+        assertTrue(expectedProduct.isPresent());
+    }
+}
