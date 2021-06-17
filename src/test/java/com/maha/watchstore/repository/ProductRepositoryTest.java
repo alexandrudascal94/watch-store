b604d2effd35bb0fa36e120390b13d23d4752f5e
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-17 12:39:24 +0200

    [test] add test findById for products with/without discount

--- src/test/java/com/maha/watchstore/repository/ProductRepositoryTest.java
+++ src/test/java/com/maha/watchstore/repository/ProductRepositoryTest.java
@@ -5,12 +5,14 @@ import com.maha.watchstore.respository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProductRepositoryTest {
 
     @Autowired
@@ -28,5 +30,19 @@ public class ProductRepositoryTest {
         long nonExistingProductId = -1L;
         Optional<Product> expectedProduct = productRepository.findById(nonExistingProductId);
         assertTrue(expectedProduct.isEmpty());
+    }
+
+    @Test
+    void getProductById_withDiscount_should_returnProductWithDiscount() {
+        long nonExistingProductId = 1L;
+        Optional<Product> expectedProduct = productRepository.findById(nonExistingProductId);
+        assertTrue(expectedProduct.get().hasDiscount());
+    }
+
+    @Test
+    void getProductById_withoutDiscount_should_returnProduct() {
+        long nonExistingProductId = 3L;
+        Optional<Product> expectedProduct = productRepository.findById(nonExistingProductId);
+        assertTrue(expectedProduct.isPresent());
     }
 }
