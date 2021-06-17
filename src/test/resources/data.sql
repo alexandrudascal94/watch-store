6c4049bf98ea1f36b15f7cee6b1a4e4c717494c1
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-17 09:31:23 +0200

    [test] Add test data for IT test plus test db configurations

---
+++ src/test/resources/data.sql
@@ -0,0 +1,12 @@
+
+insert into discount (id, price, units) values (1, 200, 3);
+insert into discount (id, price, units) values (2, 120, 2);
+
+insert into product (id, name, price, discount_id) values (1, 'Rolex', 100, 1);
+insert into product (id, name, price, discount_id) values (2, 'Michael Kors', 80, 2);
+insert into product (id, name, price) values (3, 'Swatch', 50);
+insert into product (id, name, price) values (4, 'Casio', 30);
+
+
+
+
