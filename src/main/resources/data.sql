5975d8581c2aee7dd8b11b2384aacb7d9ce0471b
adascal <alexandru.dascal@epay.ingenico.com>
2021-06-17 12:40:56 +0200

    [dev] add test profile and only on application.yaml file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ insert into discount (id, price, units) values (2, 120, 2);
 
 insert into product (id, name, price, discount_id) values (1, 'Rolex', 100, 1);
 insert into product (id, name, price, discount_id) values (2, 'Michael Kors', 80, 2);
-insert into product (id, name, price) values (3, 'Swatch', 50);
-insert into product (id, name, price) values (4, 'Casio', 30);
+insert into product (id, name, price, discount_id) values (3, 'Swatch', 50, null);
+insert into product (id, name, price, discount_id) values (4, 'Casio', 30, null);
 
 
 
--- src/test/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists discount CASCADE
-drop table if exists product CASCADE
-
-create table discount (id bigint not null, price bigint, units integer not null, primary key (id))
-create table product (id bigint not null, name varchar(255), price bigint, discount_id bigint default null, primary key (id))
-alter table product add constraint product_discount foreign key (discount_id) references discount
